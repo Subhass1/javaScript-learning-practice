5a057fb84a64c9f7d5be417622819a0be07a49f2
Subhash Yadav <subhass7658@gmail.com>
2024-07-20 19:37:14 +0545

    Update variables.js
    Gmail changes

--- 01-basics/variables.js
+++ 01-basics/variables.js
@@ -1,5 +1,5 @@
 const accountId = 144553
-let accountEmail = "hitesh@google.com"
+let accountEmail = "subhas@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 let accountState;
@@ -7,7 +7,7 @@ let accountState;
 // accountId = 2 // not allowed
 
 
-accountEmail = "hc@hc.com"
+accountEmail = "sy@hc.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
 
