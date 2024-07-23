8966985e2dbf67bde1206f7aa0ac25805f47176f
Subhash Yadav <subhass7658@gmail.com>
2024-07-23 14:14:45 +0000

    Types of variables and syntax rules

---
+++ LearnMyself/write_syntax.js
@@ -0,0 +1,24 @@
+/*
+There are three types of variables:
+i) var
+ii) let
+iii) const : const are fixed value they can't changed
+
+****************JavaScript Values**********************
+
+The JavaScript syntax defines two types of values:
+
+i) Fixed values
+ii) Variable values
+🔴Fixed values are called Literals.
+🔴Variable values are called Variables.
+
+👍JavaScript Literals
+The two most important syntax rules for fixed values are:
+
+1. Numbers are written with or without decimals:
+Example:- 10.50 ,1001
+
+2. Strings are text, written within double or single quotes:
+For Exampele:- { "Subhash Yadav", 'Ram Yadav' }
+*/
