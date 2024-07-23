2b5763a8e3bf393b9910857d3bbed4878dd4e56b
Subhash Yadav <subhass7658@gmail.com>
2024-07-23 14:36:05 +0000

    Learning datatypes eith their values

---
+++ LearnMyself/datatypes_learn.js
@@ -0,0 +1,44 @@
+/* 
+🔴JavaScript has 8 Datatypes
+• String:- Values: ("Subhas", 'Yadav')
+• Number:- values: (20 ,30, 0.005)
+• Bigint:- values: let x = BigInt("123456789012345678901234567890");
+• Boolean:- values: (True, false)
+              let x = 5;
+              let y = 5;
+              let z = 6;
+              (x == y)       // Returns true
+              (x == z)       // Returns false
+• Undefined
+→ In JavaScript, a variable without a value, has the value undefined. The type is also undefined.
+→ let car;    // Value is undefined, type is undefined
+
+• Null
+• Symbol
+• Object:-
+→ JavaScript objects are written with curly braces {}.
+→ For examples: const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+🔴The Object Datatype
+The object data type can contain both built-in objects, and user defined objects:
+
+👉objects, arrays, dates, maps, sets, intarrays, floatarrays, promises, and more.
+
+• Arrays:-
+→ JavaScript arrays are written with square brackets.
+→ Array items are separated by commas.
+→ Examples: const cars = ["Saab", "Volvo", "BMW"];
+
+• Objects:-
+
+
+
+🔴 Type operator:-
+→ It means that it shows that 'this is which datatypes'.
+→ for examples: typeof "subhas"         // Returns "string"
+                typeof 0              // Returns "number"
+                typeof 314            // Returns "number"
+                typeof 3.14           // Returns "number"
+
+
+*/
