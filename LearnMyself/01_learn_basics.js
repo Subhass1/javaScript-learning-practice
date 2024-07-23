b22969d8baf7a627251fbddaa043150db53d0615
Subhash Yadav <subhass7658@gmail.com>
2024-07-23 13:58:55 +0000

    Learn javascript with myself

---
+++ LearnMyself/01_learn_basics.js
@@ -0,0 +1,38 @@
+/*
+ JavaScript Can Change HTML Content
+One of many JavaScript HTML methods is getElementById(). 
+
+For Example:-
+ document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+ ****=====================================================
+ Using Script Tag to write external & internal javascript:
+ ==================================================*******
+ 
+ <script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+</script>
+
+*/
+
+/*================Javascript Output=========================
+JavaScript can "display" data in different ways:
+
+1) Writing into an HTML element, using innerHTML.
+Example:{ document.getElementById("demo").innerHTML = "My First JavaScript"; }
+
+2) Writing into the HTML output using document.write().
+Example: <button type="button" onclick="document.write(5 + 6)">Try it</button>
+
+3) Writing into an alert box, using window.alert().
+For eample: window.alert(5 + 6);
+while loading a page it shows a popup message
+
+4) Writing into the browser console, using console.log().
+write code in console for testing and code is working or not.
+
+#_________________To print anything on printer______________
+JavaScript Print: 
+<button onclick="window.print()">Print this page</button>
+
+*/
