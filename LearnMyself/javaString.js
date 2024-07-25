307aee954a4e9dc8ecc2c1f932eb8b004bc2efae
Subhash Yadav <subhass7658@gmail.com>
2024-07-25 04:34:17 +0000

    Pending changes exported from your codespace

---
+++ LearnMyself/javaString.js
@@ -0,0 +1,17 @@
+/* 
+🔴In javascript we can write Quotes in Quotes.
+👉Exampel:-1) let answer2 = "He is called 'Subhash' ";
+2) let answer2 ='I am "subhas" ';
+
+
+⇉ Template Strings ●
+👉let text = `He's often called "Subhash"`;
+
+🔴Find length from text.
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+*/
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let length = text.length;
+console.log("text" .length());
