c409953d9ca6f87d254c2c76401f6b3f27c34a2d
Subhash Yadav <subhass7658@gmail.com>
2024-10-13 18:52:37 +0000

    Pratice array question

---
+++ array-ques/01-ques.js
@@ -0,0 +1,8 @@
+// ✍️ Create an array of 5 numbers and log the third number in the console.
+let array = [10, 20, 30, 40, 50];
+console.log(array[2]);
+
+// ✍️ Add a number to the end of the array and log the updated array.
+array.push(60);
+console.log(array);
+console.log(array[5]);
