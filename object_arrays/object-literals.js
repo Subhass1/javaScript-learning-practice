f71464a173b1e1a452856a6941e2a535c4a3b04e
Subhash Yadav <subhass7658@gmail.com>
2024-10-13 05:39:55 +0000

    Object Literals Concept

---
+++ object_arrays/object-literals.js
@@ -0,0 +1,11 @@
+//Object Literals
+let person = {
+    myName: "Subhash",
+    age: 22,
+    isStudent: false,
+    sayHello: function (){
+        // console.log("Hello, my name is "+ this.myName);
+    }
+    
+};
+console.log(person.myName);
