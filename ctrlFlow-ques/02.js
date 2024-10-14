1bf22ab3379a16d88fe561bcae0e2010186be7f9
Subhash Yadav <subhass7658@gmail.com>
2024-10-14 19:42:20 +0000

    updates

---
+++ ctrlFlow-ques/02.js
@@ -0,0 +1,9 @@
+//Write a function that checks if a given number is even or odd.
+let = prompt("Enter any number to find odd or even: ");
+function oddEven(){
+    if(num %2 == 0){
+        console.log(`${num} is a Even Number.`);
+    }else{
+        console.log(`${num} is a Odd Number.`);
+    }
+}
