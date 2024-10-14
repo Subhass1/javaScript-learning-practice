93fcfc27b72f5829b44b05d417b82041a9319bc3
Subhash Yadav <subhass7658@gmail.com>
2024-10-14 15:55:27 +0000

    Updates

--- ctrlFlow-ques/01.js
+++ ctrlFlow-ques/01.js
@@ -1,15 +1,15 @@
 // If-else basics: Write a program that checks if a number is positive, negative, or zero.
-let num = prompt("Enter any numbers to check: ");
+let num = parseInt(prompt("Enter any numbers to check: "));
 
 if(num === 0){
-    console.log(`${num} is a Zero.`);
+    document.write(`${num} is a Zero.`);
 
-} else if (num < 0 ){
-    console.log(`${num} is a Positive Number.`);
+} else if (num > 0 ){
+    document.write(`${num} is a Positive Number.`);
 
-} else if(num > 0){
-    console.log(`${num} is a Negative number.`);
+} else if(num < 0){
+    document.write(`${num} is a Negative number.`);
 
 } else{
-    console.log(`${num} this is not valid number.`)
+    document.write(`${num} this is not valid number.`)
 }
