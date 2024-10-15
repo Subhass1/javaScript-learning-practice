ecd5b4b0ba77600816c57dfa1a3ee2c12a07fae0
Subhash Yadav <subhass7658@gmail.com>
2024-10-15 14:31:02 +0000

    Learning Multidimensional Array

---
+++ object_arrays/multidimensional.js
@@ -0,0 +1,14 @@
+// it is also called array inside array
+let fruits = [
+    ["Apple", 50],
+    ["Mango", 120],
+    ["Banana", 80],
+    ["Grapes", 75],
+];
+console.table(fruits);
+
+//Accessing Elements
+console.log(fruits [1] [0]);
+
+fruits.push(["pomogenerate", 300]);
+console.table(fruits);
