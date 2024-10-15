cd97d4608c3d14d2a857d8200fee0f9ca7d51a3c
Subhash Yadav <subhass7658@gmail.com>
2024-10-15 10:40:24 +0000

    Very easy part question no 8

---
+++ ctrlFlow-ques/08.js
@@ -0,0 +1,11 @@
+// Use a while loop to calculate the sum of numbers from 1 to 5.
+
+let sum = 0;
+let i = 1;
+
+while (i <= 5) {
+    sum += i;
+    i++;
+}
+
+console.log("Sum of the number from 1 to 5: " +sum);
