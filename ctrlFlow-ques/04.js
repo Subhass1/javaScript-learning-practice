cad6b4bebfd7d2697c82d72c65a48ec2c4b34acf
Subhash Yadav <subhass7658@gmail.com>
2024-10-15 09:26:57 +0000

    Question Number 4 and 5 pratice

---
+++ ctrlFlow-ques/04.js
@@ -0,0 +1,5 @@
+// Write a loop that prints numbers from 1 to 10.
+
+for(let i = 1; i<= 10; i++){
+    document.write(" "+i);
+}
---
+++ ctrlFlow-ques/05.js
@@ -0,0 +1,7 @@
+// Write a program that checks if a person is eligible to vote (age 18 or older).
+let age = parseInt(prompt("Enter Your age: "))
+if (age >= 18) {
+    console.log("Your are eligible for vote.")
+} else{
+    console.log("Your are minor.")
+}
