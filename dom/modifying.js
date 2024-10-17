fc7d262e48a6f8cf65d6df552100a36cd87ca84f
Subhash Yadav <subhass7658@gmail.com>
2024-10-17 08:33:15 +0000

    changing

--- dom/modifying.js
+++ dom/modifying.js
@@ -1,3 +1,9 @@
+/*
+Explanation:
+The getElementById() method selects the element with the id of "demo".
+The innerHTML property modifies the content of the element.
+*/
+
 function changeText(){
     document.getElementById("Demo").innerHTML = "My name is subhash yadav";
 }
