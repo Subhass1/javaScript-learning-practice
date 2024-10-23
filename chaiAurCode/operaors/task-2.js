bfc05754a6c3347b3942cddd0abc4165c3206244
Subhash Yadav <subhass7658@gmail.com>
2024-10-23 17:32:58 +0000

    chai aur code/ operators / task-2

---
+++ chaiAurCode/operaors/task-2.js
@@ -0,0 +1,8 @@
+// Write a program to subtract to two numbers and log the result to the console.
+
+function subtractNumbers(num1, num2){
+    let subtract = num1 - num2;
+    console.log(`the subtract of two numbers a:${a} and b:${b} is: ` + subtract);
+}
+
+subtractNumbers(20, 10);
