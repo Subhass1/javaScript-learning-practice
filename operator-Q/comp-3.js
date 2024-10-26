ed5eaca2841c946e1eea3eb245a2ada51b2d215b
Subhash Yadav <subhass7658@gmail.com>
2024-10-26 11:22:21 +0000

    chai aur code/operators / comparision third question is solved.

---
+++ operator-Q/comp-3.js
@@ -0,0 +1,15 @@
+// write a program to compare two numbers using == and === and log the result to the console.
+let a = 10;
+let b = "10";
+
+if (a == b){
+    console.log('The a and b is equal');
+} else {
+    console.log('The number a and b is not equal.');
+}
+
+// now , the part of x === z
+let x = 10;
+let y = 10;
+
+console.log("The value of x and y is completely equal.");
