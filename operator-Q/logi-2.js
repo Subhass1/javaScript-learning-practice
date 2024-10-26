212037b516e1bff4027506dd7b3f1db4dcc55802
Subhash Yadav <subhass7658@gmail.com>
2024-10-26 13:17:11 +0000

    chai aur code/ operator/logical second question solved.

---
+++ operator-Q/logi-2.js
@@ -0,0 +1,4 @@
+// write a program that uses the && operatort to combine two conditions and log the result to the console.
+let a = 10;
+let b =20;
+console.log(a > b || a < b );
---
+++ operator-Q/logi-3.js
@@ -0,0 +1,3 @@
+// write a program that uses the && operatort to combine two conditions and log the result to the console.
+let a = -10;
+console.log(!a);
