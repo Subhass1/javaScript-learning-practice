57d292bc33b23f94cff29bfd4a094ecede768f58
Subhash Yadav <subhass7658@gmail.com>
2024-10-26 10:54:02 +0000

    Chai aur code /operators/ assignment first question is solved.

--- operator-Q/assign-1.js
+++ operator-Q/assign-1.js
@@ -1,2 +1,12 @@
 // Use the += operator to add a number to a variable and log the result to the console.
 
+let a = 5;
+console.log(a += 2);
+
+// Trying with function
+function assignVariable(b){
+    return b += 2;
+}
+let b = 5;
+let result = assignVariable(b);
+console.log(result);
