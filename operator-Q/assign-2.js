5f23b029d5a3878d80653f43c83eac93a960b83c
Subhash Yadav <subhass7658@gmail.com>
2024-10-26 11:00:24 +0000

    Chai aur code/operators/ assignment operator second question is solved.

---
+++ operator-Q/assign-2.js
@@ -0,0 +1,10 @@
+// use the -= operator to subtract the number from a variable and log the result to the console.
+
+let subtract = 5;
+console.log(subtract -= 3);
+
+function check(a){
+    let subtraction = a -= 3;
+    console.log(subtraction);
+}
+check(5)
