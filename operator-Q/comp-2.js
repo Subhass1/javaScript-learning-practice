23e2d27187528dbecf8ebb07c04c126b0e780e28
Subhash Yadav <subhass7658@gmail.com>
2024-10-26 11:15:33 +0000

    Chai aur code/operator-Q/comp-2 is solved.

---
+++ operator-Q/comp-2.js
@@ -0,0 +1,13 @@
+// write a program to compare two numbers using >= and <= and log the result to the console.
+
+let num1 = 10;
+let num2 = 20;
+
+
+if (num1 >= num2){
+    console.log(`The num1 ${num1} is grater than b ${num2}`);
+} else if(num1 <= num2){
+    console.log(`The num1 ${num1} is smaller than b ${num2}`);
+} else{
+    console.log("Enter valid number: ")
+}
