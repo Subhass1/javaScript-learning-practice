d31e0993b6938278f8b6dbe5e8869bbad71b01e2
Subhash Yadav <subhass7658@gmail.com>
2024-10-26 11:07:16 +0000

    Chai aur code/ operator/ comparision first question is solved

---
+++ operator-Q/comp-1.js
@@ -0,0 +1,11 @@
+// write a program to compare two numbers using > or < and  log the result to the console.
+let a = 20;
+let b = 10;
+
+if (a > b){
+    console.log("yes, a is greater than b")
+} else if(a < b){
+    console.log("yes, a is smaller than b")
+} else{
+    console.log("Enter valid number: ")
+}
