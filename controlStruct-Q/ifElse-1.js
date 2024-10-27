1484dc3b6c5f823a3dd6b5ba7b3c622315a05e5e
Subhash Yadav <subhass7658@gmail.com>
2024-10-27 03:22:05 +0000

    chai aur code/ control structure / if else both question is solved

---
+++ controlStruct-Q/ifElse-1.js
@@ -0,0 +1,13 @@
+// write a program to check if a number is positive, negative or zero and log the result to the console.
+
+let num = -20;
+
+if(num > 0){
+    console.log(`The number ${num} is positive.`);
+} else if(num < 0 ){
+    console.log(`The number ${num} is Negative.`);
+} else if(num === 0){
+    console.log(`The number ${num} is zero.`);
+} else {
+    console.log(`The number ${num} is not validC.`);
+}
---
+++ controlStruct-Q/ifelse-2.js
@@ -0,0 +1,9 @@
+// write a program to check if a person is eligible to vote (age >= 18) and log the result to the console.
+
+let age = 35;
+
+if(age >= 18){
+    console.log("You are eligible for vote.")
+} else{
+    console.log("You are minor.")
+}
