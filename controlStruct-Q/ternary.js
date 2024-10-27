18bc12dd5f915a1afce95ad4316bb19a7a8acb4c
Subhash Yadav <subhass7658@gmail.com>
2024-10-27 10:19:44 +0000

    chai aur code/ control structure / ternary operator question is solved.

---
+++ controlStruct-Q/ternary.js
@@ -0,0 +1,7 @@
+// write a program to use a ternary operator to check if a number is even or odd and log the result to the console.
+
+
+let number = 10;
+
+let result = (number % 2 === 0 ) ? "Even Number" : "Odd Number";
+console.log(result);
