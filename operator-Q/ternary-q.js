c38c33c4a4f7e956198f2f5474d690a3d475d70d
Subhash Yadav <subhass7658@gmail.com>
2024-10-27 02:32:48 +0000

    chai aur code/ ternary operator is solved.

---
+++ operator-Q/ternary-q.js
@@ -0,0 +1,6 @@
+// write the program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+let a = 10;
+let b = 20;
+let result = (a > b) ? "True condition" : "wrong condition";
+console.log(result);
