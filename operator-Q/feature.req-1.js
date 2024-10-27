ecb5d58228cc499f57cd6c9eb6a7233d4c37b117
Subhash Yadav <subhass7658@gmail.com>
2024-10-27 02:45:44 +0000

    chai aur code/ operations / feature request first question is solved.

---
+++ operator-Q/feature.req-1.js
@@ -0,0 +1,12 @@
+// write a script that perform basic operation (addition, multiplication, subtraction, dividion, remainder) on two numbers and log the result.
+
+function arithmeticOper(num1, num2) {
+  
+  console.log(`the additon of two numbers ${num1}, ${num2} is ` + (num1 + num2));
+  console.log(`the additon of two numbers ${num1}, ${num2} is ` + (num1 * num2));
+  console.log(`the additon of two numbers ${num1}, ${num2} is ` + (num1 - num2));
+  console.log(`the additon of two numbers ${num1}, ${num2} is ` + (num1 / num2));
+  console.log(`the additon of two numbers ${num1}, ${num2} is ` + (num1 % num2));
+}
+
+arithmeticOper(20, 10);
