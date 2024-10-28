4da4269183651e1378074570b64c6991905fa782
Subhash Yadav <subhass7658@gmail.com>
2024-10-28 03:09:52 +0000

    While loop question is solved.

---
+++ loop-Q/while-1.js
@@ -0,0 +1,10 @@
+// Write a program to calculate to sum of numbers from 1 to 10 using a while loop.
+let i = 1;
+let sum = 0;
+
+while(i <= 10){
+    sum += i;
+    i++
+    console.log("Sum of numbers: " + sum);
+
+}
---
+++ loop-Q/while-2.js
@@ -0,0 +1,8 @@
+// write a program to print numbers from 10 to 1 using while loop.
+
+let i = 10;
+
+while(i >= 1){
+    i--
+    console.log("Print Numbers 10 to 1: " + i);
+}
